e97d5413592df7fc751d622246ebfb937eb8ce28
bonartm <malte.bonart@th-koeln.de>
2019-09-30 17:37:21 +0200

    add twitter carousel

--- gscrape/elements.py
+++ gscrape/elements.py
@@ -1,9 +1,10 @@
 from sys import platform
 from pyppeteer.element_handle import ElementHandle
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
 from pyppeteer.errors import ElementHandleError
 import abc
 from abc import abstractmethod
+from gscrape.utils import to_dict, inner_attribute, inner_attribute_all, inner_text, inner_text_all
 
 
 class SerpBlock(abc.ABC):
@@ -11,7 +12,6 @@ class SerpBlock(abc.ABC):
     async def get_instance(cls, block: ElementHandle) -> 'SerpBlock':
         classes = cls.__subclasses__()
         for cl in classes:
-                # test if element exists in page and return correct object
                 elem = await block.querySelector(cl.selector)
                 if elem is not None:
                     return cl(block)
@@ -22,14 +22,8 @@ class SerpBlock(abc.ABC):
     def __init__(self, block: ElementHandle):
         self.block= block 
 
-    async def inner_text(self, selector: str) ->  List[str]:
-        return await self.block.querySelectorAllEval(selector, 'nodes => nodes.map(node => node.innerText)')
-
-    async def inner_attribute(self, selector: str, attribute = 'href'):
-        return await self.block.querySelectorAllEval(selector, f'nodes => nodes.map(node => node.getAttribute("{attribute}"))')
-
     @abstractmethod
-    async def scrape(self):
+    async def scrape(self) -> Union[str, List[str]]:
         pass
 
 
@@ -37,76 +31,87 @@ class NotImplemented(SerpBlock):
     async def scrape(self):
         return {"NA": None}
 
+
 class PeopleAsk(SerpBlock):
     selector = '.related-question-pair'
     async def scrape(self):
-        questions = await self.inner_text('.related-question-pair')
+        questions = await inner_text_all(self.block, '.related-question-pair')
         return {"peopleasked": questions}
+
 
 class Images(SerpBlock):
     selector = '.YDJP6c'
     async def scrape(self):
-        links = await self.inner_attribute('img', 'title')
-        sources = await self.inner_attribute('img', 'src')
-        return {"images": [{"url": links[i], "imgsrc": sources[i]} for i in range(len(links))]}
+        urls = await inner_attribute_all(self.block, 'img', 'title')
+        sources = await inner_attribute_all(self.block, 'img', 'src')
+        return {"images": to_dict(
+            url=urls, source=sources
+        )}
 
 
 class InstantAnswer(SerpBlock):
     selector = '.ifM9O'
     async def scrape(self):
-        return {"instantanswer": None}
+        return {"instantanswer": await inner_text(self.block, '.Z0LcW')}
+
 
 class Videos(SerpBlock):
     selector = '.P94G9b'
     async def scrape(self):
-        links = await self.inner_attribute('g-inner-card a')
-        title = await self.inner_text('.wCIBKb')
-        source = await self.inner_text('.RgAZAc')
-        platform = await self.inner_text('.nHGuld .zECGdd')
-        time = [el.split("-")[1] for el in platform]
-        platform = [el.split("-")[0] for el in platform]
-        return {'videos': [{
-            'url':links[i],
-            'title': title[i], 
-            'source':source[i], 
-            'time':time[i], 
-            'platform':platform[i]} for i in range(len(links))]} 
+        urls = await inner_attribute_all(self.block, 'g-inner-card a')
+        titles = await inner_text_all(self.block, '.wCIBKb')
+        sources = await inner_text_all(self.block, '.RgAZAc')
+        platforms = await inner_text_all(self.block, '.nHGuld .zECGdd')
+        times = [el.split("-")[1] for el in platforms]
+        platforms = [el.split("-")[0] for el in platforms]
+        return {"videos": to_dict(
+            url=urls,title=titles,source=sources,
+            platform=platforms,time=times)}
+
 
 class OrganicResults(SerpBlock):    
     selector = '.rc'    
     async def scrape(self):
-        links = await self.inner_attribute('.rc .r > a')
-        titles = await self.inner_text('.ellip')
-        snippets = await self.inner_text('.st')
-        
-        return {'organic': [{
-            'url':links[i],
-            'title': titles[i], 
-            'snippets':snippets[i]} for i in range(len(links))]}
+        urls = await inner_attribute_all(self.block, '.rc .r > a:nth-of-type(1)')
+        titles = await inner_text_all(self.block, '.ellip')
+        snippets = await inner_text_all(self.block, '.st')
+        return {"organic": to_dict(
+            url=urls,title=titles,snippet=snippets)}
+
 
 class NewsCarousel(SerpBlock):
     selector = '.So9e7d'
     async def scrape(self):
-        links = await self.inner_attribute('.So9e7d a')
-        source = await self.inner_text('.So9e7d cite')
-        time = await self.inner_text('.So9e7d .f')
-        title = await self.inner_text('.nDgy9d')
-        return {'topstories_carousel': [{
-            'title': title[i], 
-            'url':links[i], 
-            'source':source[i], 
-            'time':time[i]} for i in range(len(links))]} 
+        urls = await inner_attribute_all(self.block, '.So9e7d a')
+        sources = await inner_text_all(self.block, '.So9e7d cite')
+        times = await inner_text_all(self.block, '.So9e7d .f')
+        titles = await inner_text_all(self.block, '.nDgy9d')
+        return {'topstories_carousel': to_dict(
+            title=titles,url=urls,source=sources,time=times)} 
 
 
 class NewsCards(SerpBlock):
     selector = '.qmv19b'
     async def scrape(self):
-        links = await self.inner_attribute('.dbsr a')
-        source = await self.inner_text('.wqg8ad')
-        time = await self.inner_text('.FGlSad span')
-        title = await self.inner_text('.nDgy9d')
-        return {'topstories_cards': [{
-            'title': title[i], 
-            'url':links[i], 
-            'source':source[i], 
-            'time':time[i]} for i in range(len(links))]} 
+        urls = await inner_attribute_all(self.block, '.dbsr a')
+        sources = await inner_text_all(self.block, '.wqg8ad')
+        times = await inner_text_all(self.block, '.FGlSad span')
+        titles = await inner_text_all(self.block, '.nDgy9d')
+        return {'topstories_cards':to_dict(
+            title=titles,url=urls,source=sources,time=times)} 
+
+
+class TwitterCarousel(SerpBlock):
+    selector = '.rQgnxe'
+    async def scrape(self):
+        title = await inner_text(self.block, '.zTpPx a')
+        url = await inner_attribute(self.block, '.zTpPx a')
+        tweets = await inner_text_all(self.block, '.YBEXSb')
+        times = await inner_text_all(self.block, '.f')
+        tweet_urls = await inner_attribute_all(self.block, '.YBEXSb > a')
+        return {'twitter_carousel': {
+            "title":title, "url": url, "tweets": to_dict(
+                tweet = tweets, url=tweet_urls, time=times
+            )}}
+
+
